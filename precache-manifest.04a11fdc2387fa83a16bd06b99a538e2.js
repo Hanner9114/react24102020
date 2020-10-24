46b5c3e2afa959cecbad95fdd7e633ad9ef56db9
Hanner Valencia <hvalenci@cuc.edu.co>
2020-10-24 17:52:31 -0500

    Updates

---
+++ precache-manifest.04a11fdc2387fa83a16bd06b99a538e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557d3809fad7fbc60c1b8df8c8da207e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621628a36599982c8d9b",
+    "url": "/static/css/main.b43051b1.chunk.css"
+  },
+  {
+    "revision": "75cb47f1d6241c196160",
+    "url": "/static/js/2.0d2c6f0f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0d2c6f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621628a36599982c8d9b",
+    "url": "/static/js/main.1297b8d4.chunk.js"
+  },
+  {
+    "revision": "8056e82b693f3030ff80",
+    "url": "/static/js/runtime-main.c51a4288.js"
+  },
+  {
+    "revision": "000590c7ce1d98749f12e02321460694",
+    "url": "/static/media/Mision.000590c7.png"
+  },
+  {
+    "revision": "9f72a0f1299e68ba1abab0be601f289c",
+    "url": "/static/media/Vision.9f72a0f1.png"
+  },
+  {
+    "revision": "b7a514d173cb1980ab149f7fa875128b",
+    "url": "/static/media/logo.b7a514d1.png"
+  },
+  {
+    "revision": "d4a97db84dddab43f98500a08951ba83",
+    "url": "/static/media/slide1.d4a97db8.PNG"
+  },
+  {
+    "revision": "379e3b3e49898adfaf7ad821a886ec80",
+    "url": "/static/media/slide2.379e3b3e.PNG"
+  }
+]);
